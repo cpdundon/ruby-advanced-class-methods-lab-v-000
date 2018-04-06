1f5e5ca8d04776e26b7ef6fb8ddd20f925054f09
Chris Dundon <chris.dundon@gmail.com>
2018-04-06 22:49:44 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,48 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create()
+    s = Song.new
+    s.save
+    s
+  end
+  
+  def self.new_by_name(name)
+    s = Song.new
+    s.name = name
+    s
+  end
+  
+  def self.create_by_name(name)
+    s = self.new_by_name(name)
+    s.save
+    s
+  end
+  
+  def self.find_by_name(name)
+    songs = self.all
+    idx = songs.index{|s| s.name == name}
+    
+    if idx == nil
+      return nil
+    end
+    
+    songs[idx]
+  end
+  
+  def self.find_or_create_by_name(name)
+    s = self.find_by_name(name)
+    
+    if s == nil
+      s = self.create_by_name(name)
+    end
+    s
+  end
+  
+  def self.alphabetical
+    songs = self.all
+    songs.sort_by{|x| x.name}
+    songs
+  end
 end
